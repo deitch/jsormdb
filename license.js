4016797a20917bfa4029913c0825408be7fb210d
(no author) <(no author)@e243cea2-de5c-0410-8794-c5592fff6054>
2009-03-03 17:32:46 +0000

    Should be in trunk
    git-svn-id: svn://localhost/jsormdb/trunk@33 e243cea2-de5c-0410-8794-c5592fff6054

---
+++ license.js
@@ -0,0 +1,16 @@
+/*
+ * Copyright © Atomic Inc 2007-2009
+ * http://jsorm.com
+ *
+ * This file contains work that is copyrighted and is distributed under one of several licenses. 
+ * You may not use, modify or distribute this work, except under an approved license. 
+ * Please visit the Web site listed above to obtain the original work and a license.
+ * 
+ * These libraries contains work written and published by Douglas Crockford www.crockford.com. 
+ * Page xii of "JavaScript: The Good Parts" ISBN 978-0-596-51774-8 explicitly states that
+ * "writing a program that uses several chunks of code from this book does not require permission."
+ * To use any code in these libraries that comes from that work requires reference to the original license.
+ * 
+ * Version: @@version@@
+ */
+
