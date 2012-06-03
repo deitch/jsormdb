71bf6d32d219e43e67f58ebf2d4ef5864cd9a633
Avi Deitcher <avi@deitcher.net>
2012-06-03 14:01:53 +0300

    Changed to MIT license and rebuilt

--- license.js
+++ license.js
@@ -2,9 +2,27 @@
  * Copyright © Atomic Inc 2007-2012
  * http://jsorm.com
  *
- * This file contains work that is copyrighted and is distributed under one of several licenses. 
- * You may not use, modify or distribute this work, except under an approved license. 
- * Please visit the Web site listed above to obtain the original work and a license.
+ Permission is hereby granted, free of charge, to any person
+ obtaining a copy of this software and associated documentation
+ files (the "Software"), to deal in the Software without
+ restriction, including without limitation the rights to use,
+ copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following
+ conditions:
+
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
+ * 
  * 
  * These libraries contains work written and published by Douglas Crockford www.crockford.com. 
  * Page xii of "JavaScript: The Good Parts" ISBN 978-0-596-51774-8 explicitly states that
