eddb117e22cea2da4002d7e75ae057a48d858cab
Avi Deitcher <avi@deitcher.net>
2012-06-03 14:29:03 +0300

    Moved build/ to lib/ ready for npm deployment

--- jakefile.js
+++ jakefile.js
@@ -5,10 +5,12 @@
  * Created by Avi Deitcher < avi@jsorm.com >
  */
 
-var src = "./src", build = "./build", sample = "./sample", license = "./license.js",
+var src = "./src", lib = "./lib", sample = "./sample", license = "./license.js",
   jsormBase = "../jsormutil/dist/jsorm-1.3-min-nolicense.js", basename = "jsormdb", testHtml = "test/test.html",
-  concat, cp, replace,
+  concat, cp, replace, nodify,
   fs = require('fs'), rimraf = require('rimraf'), jsmin = require('jsmin').jsmin, _ = require('underscore'), path = require('path'),
+  PRE = '(function(exports){',
+  POST = '}(typeof module === "undefined" || typeof module.exports === "undefined" ? (this.@@GLOBAL@@ === undefined ? this.@@GLOBAL@@ = {} : this.@@GLOBAL@@) : module.exports));',
   FILE_ENCODING = 'utf-8',
   VERSION = "1.3b";
   
@@ -27,6 +29,15 @@ cp = function(from,to) {
   fs.writeFileSync(to,fs.readFileSync(from,FILE_ENCODING),FILE_ENCODING);
 };
 
+nodify = function(filePath,globalVar) {
+  var pre,post,text;
+  post = POST.replace(/@@GLOBAL@@/g,globalVar);
+  pre = PRE.replace(/@@GLOBAL@@/g,globalVar);
+  text = fs.readFileSync(filePath,FILE_ENCODING);
+  fs.writeFileSync(filePath,[pre,text,post].join("\n"),FILE_ENCODING);
+  
+};
+
 replace = function(fileList,pattern,replace) {
   fileList = [].concat(fileList);
   _.each(fileList,function(filePath){
@@ -41,7 +52,7 @@ task('cleanbuild',['clean','build']);
 
 desc('Build the concatenated source file and minified source');
 task('build',['init'],function(){
-  var srcCode, licenseCode, srcFiles;
+  var srcCode, licenseCode, srcFiles, baseFile = lib+"/"+basename+"-src.js", minFile = lib+"/"+basename+".js";
 
   srcFiles = _.without(fs.readdirSync(src),"XmlParser.js").map(function(file){
     return(src+"/"+file);
@@ -50,31 +61,32 @@ task('build',['init'],function(){
   concat([
       license,
       jsormBase
-    ].concat(srcFiles),build+"/"+basename+"-src.js");
+    ].concat(srcFiles),baseFile);
+  nodify(baseFile,"JSORM");
 	
-	srcCode = fs.readFileSync(build+"/"+basename+"-src.js").toString();
+	srcCode = fs.readFileSync(baseFile).toString();
 	licenseCode = fs.readFileSync(license).toString();
-	fs.writeFileSync(build+"/"+basename+".js",licenseCode+"\n"+jsmin(srcCode));
+	fs.writeFileSync(minFile,licenseCode+"\n"+jsmin(srcCode));
 	replace([
-	  build+"/"+basename+"-src.js",
-	  build+"/"+basename+".js"
-	  ],"@@version@@",VERSION);
+	  baseFile,
+	  minFile
+	  ],/@@version@@/g,VERSION);
 	
-	cp(build+"/"+basename+".js",sample+"/"+basename+".js");
-	cp(build+"/"+basename+"-src.js",sample+"/"+basename+"-src.js");
+	cp(minFile,sample+"/"+basename+".js");
+	cp(baseFile,sample+"/"+basename+"-src.js");
 });
 
 
 desc('set things up for the build');
 task('init',[],function(){
-  if (!path.existsSync(build)) {
-    fs.mkdirSync(build);
+  if (!path.existsSync(lib)) {
+    fs.mkdirSync(lib);
   }
 });
 	
 desc('clean out old builds');
 task('clean',[],function(){
-  rimraf.sync(build);
+  rimraf.sync(lib);
   rimraf.sync(sample+"/"+basename+".js");
   rimraf.sync(sample+"/"+basename+"-src.js");
 });
